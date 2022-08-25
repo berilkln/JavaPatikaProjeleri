26e4e850ec2de4c52f2cb780c8ae8bce636bf531
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-25 19:57:08 +0300

    kdv hesaplama programı eklendi.

---
+++ src/Kdv.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Kdv {
+    public static void main(String[] args) {
+        double tutar, kdvOran = 0.18 ;
+        Scanner input = new Scanner(System.in);
+        System.out.print("miktarı giriniz: ");
+        tutar = input.nextDouble();
+
+        double kdvTutar = tutar *kdvOran;
+        double toplamTutar = kdvTutar + tutar;
+        System.out.println("KDV'li Tutar: " + toplamTutar);
+    }
+
+}
+
