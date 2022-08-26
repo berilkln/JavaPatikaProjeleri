28c45fbdef2c7497fda71fd50c7b1f8f6636daf7
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-26 11:38:55 +0300

    dairenin alanını ve çevresini hesaplayan program

---
+++ src/DaireAlanCevre.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DaireAlanCevre {
+    public static void main(String[] args) {
+        int r;
+        double alan, cevre;
+        double pi = 3.14;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("dairenin yarıçapını giriniz: ");
+
+        r = input.nextInt();
+
+        cevre = 2 * pi * r;
+        alan = pi * r * r;
+
+        System.out.println("cevre:" + cevre);
+        System.out.print("alan:" + alan);
+
+
+    }
+}
