20a9369e1dc5dbf3be36d054070c11fad0caa92c
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-26 10:57:30 +0300

    hipotenüs hesaplama programı eklendi.

---
+++ src/HipotenusHesaplama.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+public class HipotenusHesaplama {
+    public static void main(String[] args) {
+        int a, b;// üçgenin kenarları a-b-c
+        double c;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("üçgenin ilk kenarını giriniz: ");
+        a = input.nextInt();
+
+        System.out.print("üçgenin diger kenarını giriniz: ");
+        b = input.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.print("hipotenüs = " + c);
+
+
+
+
+
+
+
+
+    }
+}
+
