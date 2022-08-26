957896326e8547d79ab26efc4936719f48a2d69e
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-26 12:14:57 +0300

    manav programi

---
+++ src/Manav.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Manav {
+    public static void main(String[] args) {
+        double armut, elma, domates, muz, patlican, toplam;
+        int kilo;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print( " armut kaç kilo?");
+        kilo = input.nextInt();
+        armut = kilo * 2.14;
+
+        System.out.print( " elma kaç kilo?");
+        kilo = input.nextInt();
+        elma = kilo * 3.67;
+
+        System.out.print( " domates kaç kilo?");
+        kilo = input.nextInt();
+        domates = kilo * 1.11;
+
+        System.out.print( " muz kaç kilo?");
+        kilo = input.nextInt();
+        muz = kilo * 0.95;
+
+        System.out.print( " patlican kaç kilo?");
+        kilo = input.nextInt();
+        patlican = kilo * 5.00;
+
+        toplam = armut + elma + domates + muz + patlican;
+        System.out.print("toplam tutar:" + toplam +"TL");
+
+
+
+
+
+    }
+}
