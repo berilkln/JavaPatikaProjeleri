2c94d46013f63b36c16c8a760167a88754559bb0
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-26 12:01:07 +0300

    vücut kitle indeksi programı

---
+++ src/VucutKitleIndeksi.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+
+        double boy, VKI;  //vki = vücut kitle indeksi
+        int kilo;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("lütfen boyunuzu(m) giriniz: ");
+        boy = input.nextDouble();
+
+        System.out.print("lütfen kilonuzu(kg) giriniz: ");
+        kilo = input.nextInt();
+
+        VKI = kilo / (boy*boy);
+        System.out.print("Vücut Kitle İndeksiniz:" + VKI);
+
+
+
+
+
+
+
+
+
+    }
+}
