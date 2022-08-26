d3005d192e5254a21b9261e0eec6654300da188a
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-26 17:02:51 +0300

    sınıf gecme durumu hesaplayan program

---
+++ src/KosulluIfadeler/SinifGecmeDurumu.java
@@ -0,0 +1,38 @@
+package KosulluIfadeler;
+import java.util.Scanner;
+public class SinifGecmeDurumu {
+    public static void main(String[] args) {
+        int matematik, fizik, turkce, kimya, muzik;
+        double ortalama;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("matematik notu giriniz: ");
+        matematik = input.nextInt();
+
+        System.out.print("fizik notu giriniz: ");
+        fizik = input.nextInt();
+
+        System.out.print("turkce notu giriniz: ");
+        turkce = input.nextInt();
+
+        System.out.print("kimya notu giriniz: ");
+        kimya = input.nextInt();
+
+        System.out.print("muzik notu giriniz: ");
+        muzik = input.nextInt();
+
+        ortalama = (matematik + fizik + turkce + kimya + muzik) / 5.0;
+
+        if (ortalama <=55) {
+            System.out.print("sınıfta kaldınız!");
+        }
+        else {
+            System.out.print("sınıfı gectiniz!");
+        }
+
+
+
+
+    }
+}
