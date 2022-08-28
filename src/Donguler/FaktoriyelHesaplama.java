d8d6c3917066c8742f8dfa94b52a6b3be1a71e6a
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-28 14:10:30 +0300

    Faktöriyel Hesaplama

---
+++ src/Donguler/FaktoriyelHesaplama.java
@@ -0,0 +1,24 @@
+package Donguler;
+import java.util.Scanner;
+
+public class FaktoriyelHesaplama {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n;
+        int toplam = 1;
+        System.out.print("Faktöriyelini Bulmak İstediğiniz Sayıyı Giriniz: ");
+        n = input.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            toplam *= i;
+
+        }
+        System.out.print("Faktöriyel: " + toplam);
+
+
+
+    }
+}
+
