7ee38669a9c68dcf13ff307de2b0bed888748dbb
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-28 13:30:28 +0300

    4'ün katı olan sayıları toplayan program

---
+++ src/Donguler/CiftveDordunKati.java
@@ -0,0 +1,25 @@
+package Donguler;
+import java.util.Scanner;
+public class CiftveDordunKati {
+    public static void main(String[] args) {
+
+        //Tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve
+        // girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan program
+        int n;
+        int toplam = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("bir sayı giriniz: ");
+            n = input.nextInt();
+            if (n%4 == 0){
+                toplam += n;
+            }
+
+        }while (n%2 == 0);  // tek sayı giriline kadar döngü devam eder.
+
+        System.out.print("toplam:" + toplam);
+
+    }
+}
