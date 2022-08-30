5520811698af0e1639facca4de42a79a7ad1f7b6
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-30 09:55:43 +0300

    Üslü Sayı Hesaplama

---
+++ src/Donguler/UsluSayi.java
@@ -0,0 +1,26 @@
+package Donguler;
+import java.util.Scanner;
+
+public class UsluSayi {
+    public static void main(String[] args) {
+
+        // Java ile kullanıcının girdiği değerler ile üslü sayı hesaplayan program
+
+        int x, y;  // x^y
+        int toplam =1;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("üssü alınacak sayı: ");
+        x = input.nextInt();
+
+        System.out.print("üs olarak alınacak sayı: ");
+        y = input.nextInt();
+
+        for (int i= 1; i <= y; i++) {
+            toplam *= x;
+        }
+        System.out.print(toplam);
+
+
+    }
+}
