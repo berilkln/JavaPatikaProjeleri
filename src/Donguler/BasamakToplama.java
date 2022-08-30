0e7e7c6de71c0504c16d271aa1cd99bc28d03873
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-30 11:44:32 +0300

    girilen sayının basamaklarını toplayan program

---
+++ src/Donguler/BasamakToplama.java
@@ -0,0 +1,27 @@
+package Donguler;
+import java.util.Scanner;
+
+public class BasamakToplama {
+    public static void main(String[] args) {
+
+        int toplam = 0;
+        int sayi;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz :");
+        sayi = input.nextInt();
+
+
+        while (sayi != 0) {
+            toplam += (sayi % 10) ;
+            sayi /= 10;
+        }
+
+        System.out.print(toplam);
+
+
+
+
+
+    }
+}
