09213ae73a77b74ab5997badfc149c5a28080e23
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-08-31 14:53:00 +0300

    yıldızlardan elmas şekli oluşturma

---
+++ src/Donguler/Yildizlar.java
@@ -0,0 +1,40 @@
+package Donguler;
+import java.util.Scanner;
+
+public class Yildizlar {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n ; i++) {
+
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+
+        for (int i = n-1; i >= 1; i--) {
+
+            for (int j = 1; j <= (n-i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+
+
+
+    }
+}
+
+
