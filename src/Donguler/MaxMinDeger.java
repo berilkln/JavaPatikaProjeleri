8eb6979fbc6218edb3bbdbcfa9c688ec3b9beecf
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-09-08 11:26:58 +0300

    girilen sayılar arasında en büyük ve en küçük değeri bulan program

---
+++ src/Donguler/MaxMinDeger.java
@@ -0,0 +1,39 @@
+package Donguler;
+import java.util.Scanner;
+
+public class MaxMinDeger {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("sayı adedini giriniz: ");
+        int sayi = input.nextInt();
+
+
+        int max =1;
+        int min =1;
+
+        for (int i = 1; i <= sayi; i++) {
+            System.out.print(i + ".sayıyı giriniz: ");
+             int number = input.nextInt();
+
+            if (i == 1) {
+                max = number;
+                min = number;
+            }
+
+            if (number> max) {
+                max = number;
+            }
+
+            if (number < min) {
+                min = number;
+            }
+
+
+
+        }
+        System.out.println("max: " + max);
+        System.out.print("min: " + min);
+
+    }
+}
