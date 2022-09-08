3190764ab3e91c453fde255c2fb01ef921f3469d
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-09-08 15:09:19 +0300

    fibonacci sayılarını ekrana yazdıran program

---
+++ src/Donguler/Fibonacci.java
@@ -0,0 +1,27 @@
+package Donguler;
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz: ");
+        int n = input.nextInt();
+
+        int n1= 0, n2 = 1, n3;
+
+        System.out.print(n1 + " " + n2);// 0 ve 1 degerini başa yazdırır
+
+
+        for (int i= 2; i<n; i++) {
+           n3 = n1 + n2;
+           System.out.print(" " + n3);
+           n1 = n2;
+           n2 = n3;
+
+       }
+
+
+
+    }
+}
