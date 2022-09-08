1d9da4affa989c8541e003dd2d5cf6f098f8a7b0
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-09-08 13:52:06 +0300

    yıldızlardan ters üçgen oluşturma

---
+++ src/Donguler/TersUcgen.java
@@ -0,0 +1,24 @@
+package Donguler;
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("basamak sayısı giriniz: ");
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k=2*n-1; k >= (2*i+1); k--) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+    }
+}
