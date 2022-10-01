13bd33a77f1768c4354ddb8c3262b422cbf22d56
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-10-01 15:30:35 +0300

    Recursive metotlar ile fibonacci hesabı

---
+++ src/Metotlar/RecursiveFibonacci.java
@@ -0,0 +1,22 @@
+package Metotlar;
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        // 1 1 2 3 5 8 13 21
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("istediğiniz fibonacci basamağını girin: ");
+        int n = input.nextInt();
+        System.out.print(fib(n));
+
+    }
+}
