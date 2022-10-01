6d1951a66761ecfb48d19f0cd85638ba2b20e6a6
Beril Kartal <berilk.dev@gmail.comgit config --global user.name Beril>
2022-10-01 13:35:46 +0300

    Bir sayının palindrom sayı olup olmadığını bulan program

---
+++ src/Metotlar/PalindromSayilar.java
@@ -0,0 +1,36 @@
+package Metotlar;
+import java.util.Scanner;
+
+public class PalindromSayilar {
+
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+
+        //reverseNumber: girilen sayının tersini atadığımız sayı
+        //last number: sayının son basamağını tutan değişken
+
+        while (temp != 0) {
+            lastNumber = temp % 10;   //sayının son basamağını alıyoruz
+            reverseNumber = (reverseNumber *10) + lastNumber;
+            temp /= 10;  //gecici sayımızı bir basamak azaltıp başa dönüyoruz
+        }
+
+        if (number == reverseNumber) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz: ");
+        int number = input.nextInt();
+
+        System.out.println(isPalindrom(number));
+
+    }
+}
